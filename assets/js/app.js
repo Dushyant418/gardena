0cac32d37736b00a82838941052d3bfc05cef7ff
Dushyant418 <dushyantlochab335@gmail.com>
2024-05-01 09:17:31 +0530

    updates done

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,3 +90,42 @@ $('.small_container').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
 });
+
+window.onload = function () {
+    setTimeout(function () {
+        var loader = document.querySelector('.preloader-container');
+        loader.classList.add("active_new");
+        //  $('.preloader').addClass('active');
+        // Display your page content after preloader animation completes
+        // document.body.style.overflow = 'auto'; // Restore scrolling
+    }, 4000); // Adjust the duration as needed
+
+    // Apply animations to each span element with a delay
+    const spans = document.querySelectorAll('.preloader-text span');
+    spans.forEach((span, index) => {
+        setTimeout(() => {
+            span.style.opacity = '1';
+            span.style.animation = ' expandWidth 1000s forwards, fadeOut 500s forwards'; // Add fade-out animation
+        }, index * 200); // Adjust the delay between each letter's animation
+    });
+};
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
